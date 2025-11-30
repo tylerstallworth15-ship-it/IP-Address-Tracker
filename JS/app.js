5ba9609ca9f96f37fbf88595e4817a9844c564cd
Tyler Glen <tylerstallworth15@gmail.com>
2025-11-30 16:05:09 -0500

    jsdraft

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,22 @@
-$(document).foundation()
+const API_key = 'at_8FPC9eMlN6TiaN4yDTyLP0aCsAKZ8';
+const API_URL = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_8FPC9eMlN6TiaN4yDTyLP0aCsAKZ8';
+
+const form = document.getElementById('ip-form');
+const input = document.getElementById('ip-input');
+
+const ipText = document.getElementById('ip-value');
+const locationText = document.getElementById('location-value');
+const timezoneText = document.getElementById('timezone-value');
+const ispText = document.getElementById('isp-value');
+
+
+let map;
+let marker;
+
+function updateMap(lat, lng) {
+    if (!map) {
+        map = L.map('map').setView([lat, lng], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; OpenStreetMap contributors'
+        }).addTo(map);
